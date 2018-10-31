dc6116a273e9fbb6105c1dd662c0e9443c1557c8
Rallph <rallph@protonmail.com>
2018-10-31 00:46:46 -0500

    add API urls

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const quotes = ['hello', 'world', 'it', 'is', 'nice', 'to', 'meet', 'you'];
 
+const apiUrl = 'http://api.forismatic.com/api/1.0/';
+const apiParamss = '?method=getQuote&format=json&lang=en';
+
 
 let quote = document.querySelector('#quote');
 let quoteButton = document.querySelector('#quoteButton');
