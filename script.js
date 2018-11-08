42e60fa7da93a93ad61e0f7bea66f4aa3f58d3aa
Rallph <rallph@protonmail.com>
2018-11-08 12:24:12 -0600

    find new API, print out quote and author

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const quotes = ['hello', 'world', 'it', 'is', 'nice', 'to', 'meet', 'you'];
 
-const apiUrl = 'http://api.forismatic.com/api/1.0/';
-const apiParamss = '?method=getQuote&format=json&lang=en';
+const apiUrl = 'https://talaikis.com/api/quotes/random/';
+const apiParamss = '';
 
 
 let quote = document.querySelector('#quote');
 let quoteButton = document.querySelector('#quoteButton');
+let author = document.querySelector('#author');
 
 quoteButton.addEventListener('click', function(e) {
     
-    randomIndex = Math.floor(Math.random() * quotes.length);
+    /*randomIndex = Math.floor(Math.random() * quotes.length);
     
-    quote.innerHTML = quotes[randomIndex];
+    quote.innerHTML = quotes[randomIndex];*/
+
+    fetch(apiUrl).then((res) => res.json()).then((jsonres) => {
+
+        quote.innerHTML = `<strong>${jsonres.quote}</strong>`;
+        author.innerHTML = `<i>${jsonres.author}</i>`;
+        
+    });
 
 });
